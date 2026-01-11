44c269250e5686ae1373546aa7d4399223785e15
sribinu <sribinukaran@gmail.com>
2026-01-11 18:38:28 +0530

    1 array program

--- src/array/CommonElements.java
+++ src/array/CommonElements.java
@@ -4,22 +4,22 @@ import java.util.Scanner;
 
 /**
  * ----------------------------------------------------------
- * Program No   : ARR17
- * Question     : Merge two arrays into a single array.
- * Title        : MergeTwoArrays.java
+ * Program No   : ARR18
+ * Question     : Find common elements between two arrays.
+ * Title        : CommonElements.java
  * Author       : Sribinu P
  * Created On   : 10-01-2026
- * Topic        : Array (Merge Two Array)
+ * Topic        : Array (Common Elements)
  * Difficulty   : Easy
- * Description  : This program reads two arrays and merges them into one single array by copying elements one after another.
+ * Description  : This program finds and prints the common elements present in both arrays by comparing each element of the first array with every element of the second array.
  * ----------------------------------------------------------
  */
 
-public class MergeTwoArrays {
+public class CommonElements {
 
     public static void main(String[] args) {
-        // Program ARR15: MergeTwoArrays
-        // Copy first array elements, then second array elements
+        // Program ARR1: CommonElements
+        // Compare each element of first array with all elements of second array.
         Scanner scan = new Scanner(System.in);
 
         System.out.print("Enter size of first array: ");
@@ -30,7 +30,6 @@ public class MergeTwoArrays {
 
         int[] arr1 = new int[n1];
         int[] arr2 = new int[n2];
-        int[] merged = new int[n1 + n2];
 
         System.out.println("Enter first array elements:");
         for (int i = 0; i < n1; i++) {
@@ -42,36 +41,34 @@ public class MergeTwoArrays {
             arr2[i] = scan.nextInt();
         }
 
-        int index = 0;
-
+        System.out.print("Common Elements: ");
         for (int i = 0; i < n1; i++) {
-            merged[index++] = arr1[i];
+            for (int j = 0; j < n2; j++) {
+                if (arr1[i] == arr2[j]) {
+                    System.out.print(arr1[i] + " ");
+                    break; // avoid duplicate
+                }
+            }
         }
 
-        for (int i = 0; i < n2; i++) {
-            merged[index++] = arr2[i];
-        }
-
-        System.out.println("Merged Array: ");
-        for (int a : merged) {
-            System.out.print(a + " ");
-        }
         scan.close();
     }
 }
 
 /* Output:
-    Enter size of first array: 3
-    Enter size of second array: 4
+    Enter size of first array: 5
+    Enter size of second array: 5
     Enter first array elements:
     1
     2
     3
-    Enter second array elements:
     4
     5
-    5
-    6
-    Merged Array:
-    1 2 3 4 5 5 6
+    Enter second array elements:
+    2
+    3
+    4
+    2
+    3
+    Common Elements: 2 3 4
  */
