e8c006b052b952ceb5926d8b06c840d29549af49
sribinu <sribinukaran@gmail.com>
2026-01-14 22:45:39 +0530

    1 array program

---
+++ src/array/FrequencyOfElements.java
@@ -0,0 +1,60 @@
+package array;
+
+import java.util.Scanner;
+
+/**
+ * ----------------------------------------------------------
+ * Program No   : ARR21
+ * Question     : Count frequency of each element in the array.
+ * Title        : FrequencyOfElements.java
+ * Author       : Sribinu P
+ * Created On   : 14-01-2026
+ * Topic        : Array (Count Frequency of Elements)
+ * Difficulty   : Medium
+ * Description  :
+ * ----------------------------------------------------------
+ */
+
+public class FrequencyOfElements {
+
+    public static void main(String[] args) {
+        // Program ARR21: FrequencyOfElements
+        Scanner scan = new Scanner(System.in);
+
+        System.out.print("Enter array size: ");
+        int n = scan.nextInt();
+
+        int[] arr = new int[n];
+        boolean[] visited = new boolean[n];
+
+        System.out.println("Enter array elements:");
+        for (int i = 0; i < n; i++) {
+            arr[i] = scan.nextInt();
+        }
+
+        System.out.println("\nElement : Frequency");
+
+        for (int i = 0; i < n; i++) {
+
+            if (visited[i])
+                continue;
+
+            int count = 1;
+
+            for (int j = i + 1; j < n; j++) {
+                if (arr[i] == arr[j]) {
+                    count++;
+                    visited[j] = true;
+                }
+            }
+
+            System.out.println(arr[i] + " : " + count);
+        }
+
+        scan.close();
+    }
+}
+
+/* Output:
+
+ */
