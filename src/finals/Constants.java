8973308505480cbc803ec5eb544db24e133d6e22
sribinu <sribinukaran@gmail.com>
2026-04-05 22:29:14 +0530

    1 final keyword program

---
+++ src/finals/Constants.java
@@ -0,0 +1,29 @@
+package finals;
+
+/**
+ * ----------------------------------------------------------
+ * Program No   : FIN5
+ * Question     : Create a class with a static final variable (constant) and display it using the class name.
+ * Title        : Constants.java
+ * Author       : Sribinu P
+ * Created On   : 05-04-2026
+ * Topic        : final (constant)
+ * Difficulty   : Easy
+ * Description  : Create a class with a static final variable (constant) and display it using the class name.
+ * ----------------------------------------------------------
+ */
+
+public class Constants {
+
+    static final double PI = 3.14159;
+
+    public static void main(String[] args) {
+        // Program FIN5: Constants
+        // static final = constant → access using ClassName.variable
+        System.out.println("Value of PI: " + Constants.PI);
+    }
+}
+
+/* Output:
+    Value of PI: 3.14159
+ */
